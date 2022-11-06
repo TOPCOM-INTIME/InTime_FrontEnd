5ead86f2a11cdbdfa0533b6922769e2103621189
kkanghh1014 <attsky2@gmail.com>
2022-11-06 16:38:06 +0900

    Login&Pattern

---
+++ Intime/components/PatternList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import PatternListItem from './PatternListItem';
+
+function PatternList({logs, onScrolledToBottom, ListHeaderComponent}) {
+  const onScroll = e => {
+    if (!onScrolledToBottom) return;
+    const {contentSize, layoutMeasurement, contentOffset} = e.nativeEvent;
+    const distanceFromBottom =
+      contentSize.height - layoutMeasurement.height - contentOffset.y;
+
+    if (
+      distanceFromBottom < 72 &&
+      contentSize.height > layoutMeasurement.height
+    ) {
+      onScrolledToBottom(true);
+    } else {
+      onScrolledToBottom(false);
+    }
+  };
+
+  return (
+    <FlatList
+      data={logs}
+      style={styles.block}
+      renderItem={({item}) => <PatternListItem log={item} />}
+      keyExtractor={log => log.id}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      onScroll={onScroll}
+      ListHeaderComponent={ListHeaderComponent}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {flex: 1},
+  separator: {
+    backgroundColor: '#e0e0e0',
+    height: 1,
+    width: '100%',
+  },
+});
+
+export default PatternList;
--- Intime/screens/RootStack.js
+++ Intime/screens/RootStack.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainTab from './MainTab';
 import SignInScreen from './SignInScreen';
+import PatternCreateScreen from './PatternCreateScreen';
 import authStorage from '../stroages/authStorage';
 import {useUserContext} from '../contexts/UserContext';
 
@@ -29,6 +30,11 @@ function RootStack() {
           <Stack.Screen
             name="MainTab"
             component={MainTab}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="write"
+            component={PatternCreateScreen}
             options={{headerShown: false}}
           />
         </>
