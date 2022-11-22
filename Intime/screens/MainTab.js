8abf43d74033db7627b8cb6d433655fbb1e0555b
King-Hot <asdfg7123@gmail.com>
2022-11-22 17:16:49 +0900

    Community Screen add, logic need

--- Intime/screens/MainTab.js
+++ Intime/screens/MainTab.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomeStack from './HomeStack';
 import PatternScreen from './PatternScreen';
 import Map_Marker from './Map_Marker';
 import ScheduleList from './ScheduleListScreen';
 import ListScreen from './ListScreen';
+import CommunityScreen from './CommunityScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -21,15 +22,15 @@ function MainTab() {
         name="알람"
         component={ScheduleList}
         options={{
-          tabBarIcon: ({color}) => <Icon name="home" size={24} color={color} />,
+          tabBarIcon: ({ color }) => <Icon name="home" size={24} color={color} />,
         }}
       />
       <Tab.Screen
         name="친구"
         HomeStack
-        component={HomeStack}
+        component={CommunityScreen}
         options={{
-          tabBarIcon: ({color}) => (
+          tabBarIcon: ({ color }) => (
             <Icon name="people" size={24} color={color} />
           ),
         }}
@@ -38,7 +39,7 @@ function MainTab() {
         name="패턴"
         component={PatternScreen}
         options={{
-          tabBarIcon: ({color}) => (
+          tabBarIcon: ({ color }) => (
             <Icon name="event" size={24} color={color} />
           ),
         }}
@@ -47,7 +48,7 @@ function MainTab() {
         name="리스트"
         component={ListScreen}
         options={{
-          tabBarIcon: ({color}) => (
+          tabBarIcon: ({ color }) => (
             <Icon name="table-rows" size={24} color={color} />
           ),
         }}
