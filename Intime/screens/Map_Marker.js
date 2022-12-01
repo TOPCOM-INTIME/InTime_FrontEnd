8cada84706a3960ba3102cf47f2952ca69c84f0a
King-Hot <asdfg7123@gmail.com>
2022-12-01 19:20:18 +0900

    make Scheduleandmap

--- Intime/screens/Map_Marker.js
+++ Intime/screens/Map_Marker.js
@@ -1,31 +1,293 @@
-import React from 'react';
-import {View, Button, RefreshControl} from 'react-native';
-import MapMake from '../components/MapMake';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, Image, PermissionsAndroid } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import axios from 'axios';
+import BackgroundService from 'react-native-background-actions';
+import { useUserContext } from '../contexts/UserContext';
 
-const wait = timeout => {
-  return new Promise(resolve => setTimeout(resolve, timeout));
+
+const options = {
+  taskName: '위치',
+  taskTitle: '위치 전송기',
+  taskDesc: '내 위치를 주기적으로 파트너에게 알려주는 중...',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  color: '#ff00ff',
+  linkingURI: 'Intime://', // See Deep Linking for more info
+  parameters: {
+    delay: 1000,
+  },
 };
 
-function Map_Marker() {
-  const [refresh, setRefresh] = React.useState(false);
-  const pushMe = React.useCallback(() => {
-    setRefresh(true);
-    wait(1000).then(() => setRefresh(false));
-  }, []);
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+
+const Map_Marker = () => {
+  const { user, setUser } = useUserContext();
+  const [location, setLocation] = useState(false);
+  const [position, setPosition] = useState({
+    latitude: 37.266833,
+    longitude: 127.000019,
+  });
+
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      console.log('res is:', res);
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+            setPosition({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          error => {
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+        );
+      }
+    });
+    console.log(location);
+  };
+
+  const sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
+
+  const locationpostHandler = async taskDataArguments => {
+    const { delay } = taskDataArguments;
+    await new Promise(async resolve => {
+      for (let i = 0; BackgroundService.isRunning(); i++) {
+        const data = {
+          gps_x: 0.0,
+          gps_y: 0.0,
+        };
+        // console.log(i);
+        console.log(' ', i % 10);
+        if (i % 5 === 0) {
+          const result = requestLocationPermission();
+
+          result.then(res => {
+            // console.log('res is:', res);
+            if (res) {
+              Geolocation.getCurrentPosition(
+                async position => {
+                  data.gps_x = position.coords.latitude;
+                  data.gps_y = position.coords.longitude;
+                  // console.log('1', data);
+
+                  const res = await axios.post(
+                    `http://175.45.204.122:8000/api/4/location`,
+                    {
+                      body: data
+                    },
+                    {
+                      method: 'POST',
+                      headers: { Authorization: user },
+                    },
+                  );
+                  console.log('data', data)
+                  // console.log("location post");
+                  console.log(res.data);
+                },
+                error => {
+                  console.log(error.code, error.message);
+                  console.log('data', data)
+                  setLocation(false);
+                },
+                { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+              );
+            }
+          });
+          // console.log('2', data);
+          // const res = await axios.post(
+          //   `http://175.45.204.122:8000/api/7/location`, data,
+          //   {
+          //     headers: { Authorization: user },
+          //   },
+          // );
+          // console.log("location post");
+          // console.log(res.data);
+
+        }
+        if (i === 1000) {
+          await BackgroundService.stop();
+        }
+        await sleep(delay);
+      }
+    });
+
+  }
+
+  const backgroundHandler = async () => {
+    console.log('hi');
+    await BackgroundService.start(locationpostHandler, options);
+  };
+
+  // let getdata = {
+  //   latitude: 0.0,
+  //   longitude: 0.0,
+  // };
+
+  const [getdata, setgetdata] = useState({
+    latitude: 38.2,
+    longitude: 127.0,
+  });
+
+
+
+  const locationgetHandler = async () => {
+    const res = await axios.get(
+      `http://175.45.204.122:8000/api/4/location`,
+      {
+        headers: { Authorization: user },
+      },
+    );
+    if (res.data.gps_x == null) {
+      getdata.latitude = 38.2;
+      getdata.longitude = 127.0;
+    } else {
+      getdata.latitude = parseFloat(res.data.gps_x);
+      getdata.longitude = parseFloat(res.data.gps_y);
+
+    }
+
+    console.log('res', res);
+    console.log(getdata);
+    console.log("location get");
+    console.log(res.data);
+  }
+
+  const stopHandler = async () => {
+    await BackgroundService.stop();
+  };
+
+  const functionCombine = () => {
+    getLocation();
+    locationgetHandler();
+  };
+
+  // console.log('123123', getdata);
 
   return (
-    <>
-      <View style={{flex: 1}}>
-        <Button
-          style={{flex: 1}}
-          onPress={() => pushMe()}
-          color="#EE2F48"
-          title="파트너 위치 갱신"
-        />
-        <MapMake />
-      </View>
-    </>
-  );
-}
+    < View style={{ flex: 1, padding: 10 }
+    }>
+      <Button title="post my location in background" color="orange" onPress={backgroundHandler} />
+      <Button title="stop post my location" color="green" onPress={stopHandler} />
 
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: position.latitude,
+          longitude: position.longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        }}>
+        <Marker
+          id="0"
+          title={"내 위치"}
+          pinColor="red"
+          coordinate={{
+            latitude: position.latitude,
+            longitude: position.longitude,
+          }}
+        >
+          <Image
+            style={{ width: 26, height: 28 }}
+            source={require('../img04.gif')} />
+        </Marker>
+
+        <Marker
+          id="1"
+          title={"파트너"}
+          pinColor="blue"
+          coordinate={{
+            latitude: getdata.latitude,
+            longitude: getdata.longitude,
+          }}
+        />
+        {/* <Marker
+          id="2"
+          title={"강환희"}
+          pinColor="green"
+          coordinate={{
+            latitude: (position.latitude + 0.001),
+            longitude: (position.longitude - 0.001),
+          }}
+        />
+        <Marker
+          id="3"
+          title={"조재성"}
+          pinColor="yellow"
+          coordinate={{
+            latitude: (position.latitude - 0.001),
+            longitude: (position.longitude - 0.001),
+          }}
+        />
+        <Marker
+          id="4"
+          title={"신재혁"}
+          pinColor="purple"
+
+          coordinate={{
+            latitude: (position.latitude - 0.001),
+            longitude: (position.longitude + 0.001),
+          }}
+        /> */}
+      </MapView>
+
+      <Text style={{ color: 'green' }}>{new Date().toString()}</Text>
+      <View>
+        <Button title="location refresh" onPress={() => functionCombine()} />
+        {/* <Button title="Refresh Location" onPress={getLocation} />
+        <Button title="get Location" onPress={locationgetHandler} /> */}
+      </View>
+      <Text style={styles.Text}>Latitude: {location ? location.coords.latitude : null}</Text>
+      <Text style={styles.Text}>Longitude: {location ? location.coords.longitude : null}</Text>
+    </View >
+
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+  },
+  Text: {
+    color: '#EE2F48',
+  },
+});
 export default Map_Marker;
---
+++ Intime/screens/ScheduleandMap.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Alert,
+} from 'react-native';
+import Map_Marker from './Map_Marker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function ScheduleandMap() {
+    return (
+        <>
+            <View style={styles.container}>
+                <View >
+                    <View >
+                        <Text style={styles.sectionTitle}>일정</Text>
+                        <View
+                            style={{
+                                borderRadius: 10,
+                                borderColor: '#ED3648',
+                                borderWidth: 3,
+                                justifyContent: 'flex-start',
+                                flexDirection: 'row',
+                            }}
+                        >
+                            <Icon name="face" size={36} color={"black"} />
+                            <Icon name="face" size={36} color={"black"} />
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={{ flex: 7 }}>
+                <Map_Marker></Map_Marker>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    tasksWrapper: {
+        paddingTop: 20,
+        paddingHorizontal: 20,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    sectionTitle: {
+        fontSize: 24,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+});
+
+export default ScheduleandMap;
